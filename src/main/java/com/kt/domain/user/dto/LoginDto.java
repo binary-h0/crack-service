8b5a5b935e25a3ffcd89044c73731d0a31eab970
binary-h0 <max000807@gmail.com>
2025-02-12 16:58:19 +0900

    thrid commit

---
+++ src/main/java/com/kt/domain/user/dto/LoginDto.java
@@ -0,0 +1,16 @@
+package com.kt.domain.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDto {
+
+    @NotBlank(message = "아이디를 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
+}
---
+++ src/main/java/com/kt/domain/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.kt.domain.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kt.domain.user.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
